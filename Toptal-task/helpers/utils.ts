0b9f8ee3cf940740cc7b7450df3513e4ac847f0d
niksaklecakc060857 <148770018+niksaklecakc060857@users.noreply.github.com>
2025-08-07 11:55:13 +0300

    prepared env

--- Toptal-task/helpers/utils.ts
+++ Toptal-task/helpers/utils.ts
@@ -1,1 +1,12 @@
+export class Utils {
+  static async getRandomString(length: number) {
+    return Math.random()
+      .toString(36)
+      .substring(2, 2 + length);
+  }
 
+  //placeholder method
+  static async exampleMethod() {
+    return "example";
+  }
+}
--- Toptal-task/tests/example-test.spec.ts
+++ Toptal-task/tests/example-test.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from "@playwright/test";
+import { Utils } from "../helpers/utils";
 
 test("example test", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle("Google");
+});
+
+test("task", async ({ page }) => {
+  const result = await Utils.exampleMethod();
+  console.log(result);
 });
